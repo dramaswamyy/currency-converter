f9a94cd1f0778b133d19f088f1ed393d4c65acce
Divya Ramaswamy <dsramasw@ncsu.edu>
2022-05-04 12:27:45 -0400

    [IMPLEMENTATION] added icons

---
+++ App/constants/sizes.js
@@ -0,0 +1,3 @@
+export default {
+    arrow: 30
+};
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, SafeAreaView, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 import colors from '../constants/colors';
+import sizes from '../constants/sizes';
 
 const styles = StyleSheet.create({
-   button : {paddingHorizontal: 16, paddingVertical: 20},
-   text: {backgroundColor: colors.background, color: colors.lightAccent},
+   button : {paddingHorizontal: 16, paddingVertical: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'},
+   text: { color: colors.darkerColor},
    separator: {backgroundColor: colors.accent, height: StyleSheet.hairlineWidth, marginHorizontal: 15}
 });
 
@@ -12,11 +14,20 @@ export default function Option() {
     return (
       <SafeAreaView>
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Example 1</Text>
+          <Text style={styles.text}>Themes</Text>
+          <AntDesign name='picture' color={colors.accent} size={sizes.button} />
         </TouchableOpacity>
+
         <View style={styles.separator} />
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Example 2</Text>  
+          <Text style={styles.text}>React Native Basics</Text>
+          <AntDesign name='rightcircle' color={colors.accent} size={sizes.button} />
+        </TouchableOpacity>
+
+        <View style={styles.separator} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>React Native Example</Text>  
+          <AntDesign name='rightcircle' color={colors.accent} size={sizes.button} />
         </TouchableOpacity>
       </SafeAreaView>
     );
